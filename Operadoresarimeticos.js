db8df247aade6b7e861146df5b67ea829f96c679
Jorge <salazar107991@insuco.edu.mx>
2022-03-01 10:55:13 -0600

    ejercicio1

--- Operadoresarimeticos.js
+++ Operadoresarimeticos.js
@@ -39,3 +39,5 @@ console.log( z );
 z = o--;
 console.log( o )
 console.log( z )
+
+
---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+let A = 500, b = 600;
+let z = A * b;
+console.log( "Resultado de multiplicacion es:" + z )
+//Pre-incremento
+//z = ++A;
+console.log( A )
+console.log( z )
+//Post-incremento
+//z = b++;
+console.log( b );
+console.log( z );
+v = ++z;
+console.log( v )
+console.log( z )

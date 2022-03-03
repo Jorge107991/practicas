c22f3739c7236e044aa16c2233c0218e5c47a6ea
Jorge <salazar107991@insuco.edu.mx>
2022-03-03 11:25:42 -0600

    operadordistintoa

---
+++ operadordistintoa.js
@@ -0,0 +1,8 @@
+let a = 3, b = 2, c = "3";
+//!= Se utiliza para saber si son diferentes
+
+let z = a != c;
+console.log( z );
+
+z = a !== c;
+console.log( z );
---
+++ operadoresdecomparacion.js
@@ -0,0 +1,7 @@
+//Operador simple
+let a = 3, b = 2, c = "3";
+let z = a == c; //Se revisa el valor sin importar el tipo(a==c)
+console.log( z );
+//Comparador estricto
+z = a === c; //Revisa si los valores son iguales pero tambien que los tipos no sean iguales
+console.log( z );
